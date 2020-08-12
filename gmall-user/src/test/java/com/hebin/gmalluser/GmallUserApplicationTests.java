b09d696e7e76385234f8f1fe3e70139ee294bd39
creek91 <hebin.ck@foxmail.com>
2020-08-12 11:49:14 +0800

    2020 08 12 创建新的gmall-user工程； 完成工程的初步配置

---
+++ gmall-user/src/test/java/com/hebin/gmalluser/GmallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hebin.gmalluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
